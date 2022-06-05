4c962168f722e94123880465c7d7331ab2746657
Setsofia-lab <setsofiaeli@gmail.com>
2022-06-05 13:59:48 +0000

    Hello Api View

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -22,7 +22,7 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self,email,name, password):
         """Create and save new super user"""
-        user = self.create_user(self, email, name, password)
+        user = self.create_user( email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get (self, request, format=None):
+       """returns a lsit of API features"""
+       an_apiview =[
+          'Uses HTTP method as fucntion (get, post, patch,put, delete)'
+          'Is similar to a traditional Django View' 
+          'Give you the most control over your app logiv'
+          'Is mapped manually to urls'
+       ]
+
+       return Response({'message':'Hello', 'an_apiview': an_apiview})
